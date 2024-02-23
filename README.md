419bf365417f20b114260874f6f58e5465b0f17c
Manuel Schierenberg <manuel.schierenberg@teclead-ventures.de>
2024-02-23 16:16:03 +0100

    feat: use dotenv

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # :camera_flash: Savannah Nixon Phtography Backend
 
 Official repository for the backend of [Savannah Nixon](https://savannahnixonphotography.de).
+
+https://dbdiagram.io/d/Savannah-Nixon-65d85f285cd0412774a420f3
--- src/main/java/de/savannahnixon/backend/SavannahNixonPhotographyApplication.java
+++ src/main/java/de/savannahnixon/backend/SavannahNixonPhotographyApplication.java
@@ -2,12 +2,34 @@ package de.savannahnixon.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class SavannahNixonPhotographyApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+
+		// Set environment variables
+		System.setProperty("spring.datasource.url", dotenv.get("DATABASE_URL"));
+		System.setProperty("spring.datasource.username", dotenv.get("DATABASE_USERNAME"));
+		System.setProperty("spring.datasource.password", dotenv.get("DATABASE_PASSWORD"));
+
 		SpringApplication.run(SavannahNixonPhotographyApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE");
+			}
+		};
 	}
 
 }
